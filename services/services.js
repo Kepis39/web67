a514b4bc05c5dc5e29bf05bd5b88ba7407b64a84
Kepis39 <101018161+Kepis39@users.noreply.github.com>
2026-06-17 16:04:55 +0300

    Update services.js

--- services/services.js
+++ services/services.js
@@ -1,16 +1,16 @@
 const fanta = [
-    {img:"/kartinki/service/Icon Container (8).png",title:"Valuation Mastery",text:"Discover the true worth of your property with our expert valuation services."},
-    {img:"/kartinki/service/Icon Container (9).png",title:"Strategic Marketing",text:"Selling a property requires more than just a listing; it demands a strategic marketing approach."},
-    {img:"/kartinki/service/Icon Container (10).png",title:"Negotiation Wizardry",text:"Negotiating the best deal is an art, and our negotiation experts are masters of it."},
-    {img:"/kartinki/service/Icon Container (11).png",title:"Closing Success",text:"A successful sale is not complete until the closing. We guide you through the intricate closing process."},
-    {img:"/kartinki/service/Icon Container.png",title:"Tenant Harmony",text:"Our Tenant Management services ensure that your tenants have a smooth and reducing vacancies."},
-    {img:"/kartinki/service/Icon Container (1).png",title:"Maintenance Ease",text:"Say goodbye to property maintenance headaches. We handle all aspects of property upkeep."},
-    {img:"/kartinki/service/Icon Container (2).png",title:"Financial Peace of Mind",text:"Managing property finances can be complex. Our financial experts take care of rent collection"},
-    {img:"/kartinki/service/Icon Container (3).png",title:"Legal Guardian",text:"Stay compliant with property laws and regulations effortlessly."},
-    {img:"/kartinki/service/Icon Container (4).png",title:"Market Insight",text:"Stay ahead of market trends with our expert Market Analysis. We provide in-depth insights into real estate market conditions"},
-    {img:"/kartinki/service/Icon Container (5).png",title:"ROI Assessment",text:"Make investment decisions with confidence. Our ROI Assessment services evaluate the potential returns on your investments"},
-    {img:"/kartinki/service/Icon Container (6).png",title:"Customized Strategies",text:"Every investor is unique, and so are their goals. We develop Customized Investment Strategies tailored to your specific needs"},
-    {img:"/kartinki/service/Icon Container (7).png",title:"Diversification Mastery",text:"Diversify your real estate portfolio effectively. Our experts guide you in spreading your investments across various property types and locations"}
+    {img:"kartinki/service/Icon Container (8).png",title:"Valuation Mastery",text:"Discover the true worth of your property with our expert valuation services."},
+    {img:"kartinki/service/Icon Container (9).png",title:"Strategic Marketing",text:"Selling a property requires more than just a listing; it demands a strategic marketing approach."},
+    {img:"kartinki/service/Icon Container (10).png",title:"Negotiation Wizardry",text:"Negotiating the best deal is an art, and our negotiation experts are masters of it."},
+    {img:"kartinki/service/Icon Container (11).png",title:"Closing Success",text:"A successful sale is not complete until the closing. We guide you through the intricate closing process."},
+    {img:"kartinki/service/Icon Container.png",title:"Tenant Harmony",text:"Our Tenant Management services ensure that your tenants have a smooth and reducing vacancies."},
+    {img:"kartinki/service/Icon Container (1).png",title:"Maintenance Ease",text:"Say goodbye to property maintenance headaches. We handle all aspects of property upkeep."},
+    {img:"kartinki/service/Icon Container (2).png",title:"Financial Peace of Mind",text:"Managing property finances can be complex. Our financial experts take care of rent collection"},
+    {img:"kartinki/service/Icon Container (3).png",title:"Legal Guardian",text:"Stay compliant with property laws and regulations effortlessly."},
+    {img:"kartinki/service/Icon Container (4).png",title:"Market Insight",text:"Stay ahead of market trends with our expert Market Analysis. We provide in-depth insights into real estate market conditions"},
+    {img:"kartinki/service/Icon Container (5).png",title:"ROI Assessment",text:"Make investment decisions with confidence. Our ROI Assessment services evaluate the potential returns on your investments"},
+    {img:"kartinki/service/Icon Container (6).png",title:"Customized Strategies",text:"Every investor is unique, and so are their goals. We develop Customized Investment Strategies tailored to your specific needs"},
+    {img:"kartinki/service/Icon Container (7).png",title:"Diversification Mastery",text:"Diversify your real estate portfolio effectively. Our experts guide you in spreading your investments across various property types and locations"}
 ];
 const sprite = document.getElementById('services-top');
 const cola = document.getElementById('services-bottom');
