5989691e7261b955f50aaa01e461cf670c573711
Kepis39 <101018161+Kepis39@users.noreply.github.com>
2026-06-17 14:37:11 +0300

    Update details.js

--- Details/details.js
+++ Details/details.js
@@ -3,10 +3,10 @@ const que = [
     {title:"What documents do I need to sell my property through Estatein?",desc:"Find out about the necessary documentation for listing your property with us."},
     {title:"How can I contact an Estatein agent?",desc:"Discover the different ways you can get in touch with our experienced agents."}
 ];
-const queCard = document.getElementById('que');
+const queCard = document.getElementById('faq-grid');
 queCard.innerHTML = que.map(
-    i => `<div class="q">
-            <div class="qCont">
+    i => `<div class="faq-card">
+            <div class="faq-card-inner">
                 <h3>${i.title}</h3>
                 <p>${i.desc}</p>
                 <button>Read More</button>
