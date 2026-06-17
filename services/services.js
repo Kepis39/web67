6ba6b200e72d8a2d289e2b901bfabb5a81674cbf
Kepis39 <101018161+Kepis39@users.noreply.github.com>
2026-06-17 14:57:39 +0300

    Update services.js

--- services/services.js
+++ services/services.js
@@ -12,12 +12,12 @@ const fanta = [
     {img:"/kartinki/service/Icon Container (6).png",title:"Customized Strategies",text:"Every investor is unique, and so are their goals. We develop Customized Investment Strategies tailored to your specific needs"},
     {img:"/kartinki/service/Icon Container (7).png",title:"Diversification Mastery",text:"Diversify your real estate portfolio effectively. Our experts guide you in spreading your investments across various property types and locations"}
 ];
-const sprite = document.getElementById('tverh');
-const cola = document.getElementById('tniz');
-const sprite2 = document.getElementById('tverh2');
-const cola2 = document.getElementById('tniz2');
-const pepsi = document.getElementById('pepsii');
-const pepsi2 = document.getElementById('pepsi1');
+const sprite = document.getElementById('services-top');
+const cola = document.getElementById('services-bottom');
+const sprite2 = document.getElementById('services-top-2');
+const cola2 = document.getElementById('services-bottom-2');
+const pepsi = document.getElementById('investment-row-1');
+const pepsi2 = document.getElementById('investment-row-2');
 const spriteCard = fanta.slice(0,3);
 const colaCard = fanta.slice(3,4);
 const spriteCard2 = fanta.slice(4,7);
@@ -26,8 +26,8 @@ const pepsiCard = fanta.slice(8,10);
 const pepsiCard2 = fanta.slice(10,12);
 
 sprite.innerHTML = spriteCard.map(
-    i => `<div class="fanta">
-            <div class="fantaCont">
+    i => `<div class="service-card">
+            <div class="service-card-inner">
                 <div>
                     <img src="${i.img}" alt="">
                     <h2>${i.title}</h2>
@@ -37,8 +37,8 @@ sprite.innerHTML = spriteCard.map(
          </div>`
 ).join('');
 cola.innerHTML = colaCard.map(
-    i => `<div class="fanta">
-            <div class="fantaCont">
+    i => `<div class="service-card">
+            <div class="service-card-inner">
                 <div>
                     <img src="${i.img}" alt="">
                     <h2>${i.title}</h2>
@@ -48,8 +48,8 @@ cola.innerHTML = colaCard.map(
          </div>`
 ).join('');
 sprite2.innerHTML = spriteCard2.map(
-    i => `<div class="fanta">
-            <div class="fantaCont">
+    i => `<div class="service-card">
+            <div class="service-card-inner">
                 <div>
                     <img src="${i.img}" alt="">
                     <h2>${i.title}</h2>
@@ -59,8 +59,8 @@ sprite2.innerHTML = spriteCard2.map(
          </div>`
 ).join('');
 cola2.innerHTML = colaCard2.map(
-    i => `<div class="fanta">
-            <div class="fantaCont">
+    i => `<div class="service-card">
+            <div class="service-card-inner">
                 <div>
                     <img src="${i.img}" alt="">
                     <h2>${i.title}</h2>
@@ -70,8 +70,8 @@ cola2.innerHTML = colaCard2.map(
          </div>`
 ).join('');
 pepsi.innerHTML = pepsiCard.map(
-    i => `<div class="fanta">
-            <div class="fantaCont">
+    i => `<div class="service-card">
+            <div class="service-card-inner">
                 <div>
                     <img src="${i.img}" alt="">
                     <h2>${i.title}</h2>
@@ -81,8 +81,8 @@ pepsi.innerHTML = pepsiCard.map(
          </div>`
 ).join('');
 pepsi2.innerHTML = pepsiCard2.map(
-    i => `<div class="fanta">
-            <div class="fantaCont">
+    i => `<div class="service-card">
+            <div class="service-card-inner">
                 <div>
                     <img src="${i.img}" alt="">
                     <h2>${i.title}</h2>
