27b3275da431780f9e3b2e4ea1ba04fbb665a9d0
Kepis39 <101018161+Kepis39@users.noreply.github.com>
2026-06-17 15:57:21 +0300

    Update properties.js

--- Properties/properties.js
+++ Properties/properties.js
@@ -1,7 +1,7 @@
 const houses = [
-    {img:"/kartinki/main1/MoyaDacha.png", name:"Seaside Serenity Villa", description:"A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...", bed:"4-Bedroom", shower:"3-Bathroom"},
-    {img:"/kartinki/main1/City.png", name:"Metropolitan Haven", description:"A chic and fully-furnished 2-bedroom apartment with panoramic city views...", bed:"2-Bedroom", shower:"2-Bathroom"},
-    {img:"/kartinki/main1/VipHouse.png", name:"Rustic Retreat Cottage", description:"An elegant 3-bedroom, 2.5-bathroom townhouse in a gated community...", bed:"3-Bedroom", shower:"3-Bathroom"}
+    {img:"kartinki/main1/MoyaDacha.png", name:"Seaside Serenity Villa", description:"A stunning 4-bedroom, 3-bathroom villa in a peaceful suburban neighborhood...", bed:"4-Bedroom", shower:"3-Bathroom"},
+    {img:"kartinki/main1/City.png", name:"Metropolitan Haven", description:"A chic and fully-furnished 2-bedroom apartment with panoramic city views...", bed:"2-Bedroom", shower:"2-Bathroom"},
+    {img:"kartinki/main1/VipHouse.png", name:"Rustic Retreat Cottage", description:"An elegant 3-bedroom, 2.5-bathroom townhouse in a gated community...", bed:"3-Bedroom", shower:"3-Bathroom"}
 ];
 
 const cardDiv = document.getElementById('properties-grid');
@@ -13,9 +13,9 @@ cardDiv.innerHTML = houses.map(
                     <h3>${i.name}</h3>
                     <p>${i.description}<a href="">Read more</a></p>
                     <div class="property-features">
-                        <button><img src="/kartinki/main1/bed.png" alt="">${i.bed}</button>
-                        <button><img src="/kartinki/main1/shower.png" alt="">${i.shower}</button>
-                        <button><img src="/kartinki/main1/villa.png" alt="">Villa</button>
+                        <button><img src="kartinki/main1/bed.png" alt="">${i.bed}</button>
+                        <button><img src="kartinki/main1/shower.png" alt="">${i.shower}</button>
+                        <button><img src="kartinki/main1/villa.png" alt="">Villa</button>
                     </div>
                     <div class="property-price">
                         <div>
